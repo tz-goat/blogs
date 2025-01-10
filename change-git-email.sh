1005359241361989859b05bc168de0c1f15ae3ed
adamzhang <ztengc5@gmail.com>
2025-01-10 18:00:26 +0800

    chore: add more context to change-git-email

--- change-git-email.sh
+++ change-git-email.sh
@@ -1,3 +1,7 @@
+# Context: 
+# need to write this script cause some of my previous commits' email is wrong 
+# and Github contributions didn't recognize it
+
 git filter-branch --env-filter '
 OLD_EMAIL="adamzhang@futunn.com"
 CORRECT_EMAIL="ztengc5@gmail.com"
@@ -14,3 +18,16 @@ then
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags 
+
+
+# what is git filter-branch?
+
+
+# git filter-branch is a Git command that rewrites Git history
+# It can be used to:
+# 1. Change commit author/committer information
+# 2. Remove files from entire Git history
+# 3. Move subdirectories to their own repositories
+# 4. Make global changes across the entire repository history
+
+# GitHub now recommends using git-filter-repo instead of git filter-branch as it's faster and more powerful
